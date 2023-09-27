48448b0f02cfe8c533b938f884d74f91bab5e86f
Dominik Schneider <Dominik.Schneider@wibizu.de>
2023-09-27 15:40:13 +0200

    hehe xd

--- GraphFrontend2/CanvasGraph.cs
+++ GraphFrontend2/CanvasGraph.cs
@@ -13,45 +13,111 @@ using System.Globalization;
 
 namespace GraphFrontend2
 {
-    public enum GraphType { WeightedGraph, WeightedDirectedGraph, WeightlessGraph, WeightlessDirectedGraph }
-    internal class CanvasGraph<T>
+    public enum GraphType { Graph, DirectedGraph, WeightedGraph, WeightedDirectedGraph }
+    internal class CanvasGraph
     {
         protected Graph graph { get; set; }
         protected Canvas canvas { get; set; }
         protected Vertex? activevertex { get; set; }
         protected Edge? activeedge { get; set; }
         protected GraphType type { get; set; }
+        protected int vertexcount { get; set; }
 
         public CanvasGraph(GraphType _gtype, Canvas _canvas)
         {
             switch (_gtype)
             {
+                case GraphType.Graph:
+                    graph = new Graph();
+                    break;
                 case GraphType.WeightedGraph:
-                    graph = new WeightedGraph();
-                    break;
+                    graph = new Graph();
+                    break; 
                 case GraphType.WeightedDirectedGraph:
-                    graph = new WeightedDirectedGraph();
+                    graph = new DirectedGraph();
                     break;
-                case GraphType.WeightlessGraph:
-                    graph = new WeightlessGraph();
-                    break;
-                case GraphType.WeightlessDirectedGraph:
-                    graph = new WeightlessDirectedGraph();
+                case GraphType.DirectedGraph:
+                    graph = new DirectedGraph();
                     break;
                 default:
-                    graph = new WeightlessGraph();
+                    graph = new Graph();
                     break;
             }
             canvas = _canvas;
             activevertex = null;
             activeedge = null;
             type = _gtype;
+            vertexcount = 0;
             canvas.Children.Clear();
         }
 
         public void OnClick(Point mouse)
         {
+            if(activevertex is not null)
+            {
+                var ver = VertexOfPosition(mouse);
+                if(ver is not null && activevertex != ver)
+                {
+                    Position pos = new Position((ver.position.x + activevertex.position.x)/2, (ver.position.y + activevertex.position.y) / 2);
+                    string name = activevertex.name + ver.name;
+                    if(type == GraphType.Graph || type == GraphType.DirectedGraph)
+                    {
+                        Edge edg = new Edge(name, activevertex, ver, pos);
+                        graph.AddEdge(edg);
 
+                    }
+                    else
+                    {
+                        Edge edg = new Edge(name, activevertex, ver, 0, pos);
+                        graph.AddEdge(edg);
+                    }
+                }
+                else if(ver is not null && activevertex == ver)
+                {
+                    //Implement DoubleClick
+                }
+                else
+                {
+                    activevertex.position = new Position(mouse.X, mouse.Y);
+                    DrawGraph();
+                    activevertex = null; 
+                }
+            }
+            else if(activeedge is not null)
+            {
+                var edg = EdgeOfPosition(mouse);
+                if(edg is not null && edg == activeedge)
+                {
+                    //Implement DoubleClick
+                    if(IsPointInRectangleOfWeighting(mouse, edg))
+                    {
+
+                    }
+                }
+                else if(edg is not null)
+                {
+                    edg.position = new Position(mouse.X, mouse.Y);
+                }
+            }
+            else
+            {
+                var ver = VertexOfPosition(mouse);
+                var edg = EdgeOfPosition(mouse);
+                if(ver is not null)
+                {
+                    activevertex = ver;
+                }
+                else if(edg is not null)
+                {
+                    activeedge = edg;
+                }
+                else
+                {
+                    Vertex vnew = new Vertex("ver" + vertexcount, new Position(mouse.X, mouse.Y));
+                    graph.AddVertex(vnew);
+                    vertexcount++;
+                }
+            }
         }
 
         private bool IsPointInCircleOfVertex(Point p, Vertex u)
@@ -61,7 +127,7 @@ namespace GraphFrontend2
             if (lhs <= rhs) return true; else return false;
         }
 
-        private bool IsPointInRectangleOfEdge<T>(Point p, T e) where T: WeightedEdge, WeightlessEdge
+        private bool IsPointInRectangleOfEdge(Point p, Edge e)
         {
             string line = e.name + ": " + e.weight;
             FormattedText formatted = new FormattedText(line, CultureInfo.CurrentCulture, System.Windows.FlowDirection.LeftToRight, new Typeface(Settings1.Default.Font), Settings1.Default.Fontsize, Brushes.Black);
@@ -76,23 +142,9 @@ namespace GraphFrontend2
             if (p.X >= left && p.X <= right && p.Y <= top && p.Y >= bottom) return true; else return false;
         }
 
-        private bool IsPointInRectangleOfEdge<T>(Point p, T e) where T:WeightlessEdge
+        private bool IsPointInRectangleOfWeighting(Point p, Edge e)
         {
-            string line = e.name;
-            FormattedText formatted = new FormattedText(line, CultureInfo.CurrentCulture, System.Windows.FlowDirection.LeftToRight, new Typeface(Settings1.Default.Font), Settings1.Default.Fontsize, Brushes.Black);
-            var width = formatted.Width;
-            var height = formatted.Height;
-            var x = e.position.x;
-            var y = e.position.y;
-            var left = x - width / 2;
-            var top = y + height / 2;
-            var right = x + width / 2;
-            var bottom = y - height / 2;
-            if (p.X >= left && p.X <= right && p.Y <= top && p.Y >= bottom) return true; else return false;
-        }
-
-        private bool IsPointInRectangleOfWeighting(Point p, WeightedEdge e)
-        {
+            if (e.weight is null) return false;
             string line = e.name + ": " + e.weight;
             string weight = e.weight.ToString();
             FormattedText formattedline = new FormattedText(line, CultureInfo.CurrentCulture, System.Windows.FlowDirection.LeftToRight, new Typeface(Settings1.Default.Font), Settings1.Default.Fontsize, Brushes.Black);
@@ -119,12 +171,18 @@ namespace GraphFrontend2
             return null;
         }
 
-        private WeightedEdge EdgeOfPosition(Point p)
+        private Edge? EdgeOfPosition(Point p)
         {
             foreach(var e in graph.edges)
             {
-                if(IsPointInRectangleOfEdge(p, e))
+                if(IsPointInRectangleOfEdge(p, e)) return e;
             }
+            return null;
+        }
+
+        private void DrawGraph()
+        {
+
         }
     }
 }
